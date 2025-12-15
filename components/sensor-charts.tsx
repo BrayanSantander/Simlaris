e1d4e445a262ed8919317d217eaa4a51f475db6e
Brayan Santander <115866143+BrayanSantander@users.noreply.github.com>
2025-12-14 23:43:46 -0300

    Update sensor-charts.tsx

--- components/sensor-charts.tsx
+++ components/sensor-charts.tsx
@@ -1,27 +1,13 @@
 "use client"
 
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
-import { SensorLineChart } from "@/components/charts/SensorLineChart"
+import SensorLineChart from "@/components/charts/SensorLineChart"
 import MachinesStatusChart from "@/components/charts/MachinesStatusChart"
 
-interface SensorChartsProps {
-  machines: any[]
-}
-
-export function SensorCharts({ machines }: SensorChartsProps) {
+export function SensorCharts() {
   return (
-    <Card>
-      <CardHeader>
-        <CardTitle>Monitoreo de Sensores</CardTitle>
-      </CardHeader>
-
-      <CardContent className="grid grid-cols-1 lg:grid-cols-2 gap-6">
-        {/* Gráfico de sensores */}
-        <SensorLineChart machines={machines} />
-
-        {/* Estado de máquinas */}
-        <MachinesStatusChart machines={machines} />
-      </CardContent>
-    </Card>
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+      <SensorLineChart />
+      <MachinesStatusChart />
+    </div>
   )
 }
